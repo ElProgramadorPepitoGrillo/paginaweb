75eb011892ba658b05c215729a7f1c16894271eb
MarcoParedes <paredes.marco.jr@gmail.com>
2022-11-14 12:36:48 -0500

    blog parte 1.5

--- MiApp/Components/componente5/componente5.component.ts
+++ MiApp/Components/componente5/componente5.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-componente5',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Componente5Component implements OnInit {
 
-  constructor() { }
+  constructor(public nav:Router) { }
 
   ngOnInit(): void {
+  }
+  navegacion(){    
+    this.nav.navigate([''])
   }
 
 }
