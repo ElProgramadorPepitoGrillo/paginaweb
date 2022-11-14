2303ce5128bf144b874fd4f8fa0cda5814676040
MarcoParedes <paredes.marco.jr@gmail.com>
2022-11-14 11:58:48 -0500

    clase de navegacion

--- MiApp/src/app/app-routing.module.ts
+++ MiApp/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Componente1Component } from 'Components/componente1/componente1.component';
+import { Componente2Component } from 'Components/componente2/componente2.component';
+import { pathToFileURL } from 'url';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',component: Componente1Component
+  },
+  {
+    path:'web2',component: Componente2Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
